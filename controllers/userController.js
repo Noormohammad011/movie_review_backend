d8738df252cc74e07230f6c6d5900d058c2aff9d
Noor Mohammad <noormohammad.net1503011@gmail.com>
2023-02-09 20:29:16 +0600

    Verify user email with token

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import sendEmail, { generateOtp } from '../utils.js/sendEmail.js'
 import EmailVerificationToken from '../models/emailVerificationTokenModel.js'
+import { isValidObjectId } from 'mongoose'
 
 // @desc    Register a new user
 // @route   POST /api/v1/users
@@ -36,7 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
   try {
     await sendEmail({
       email: user.email,
-      subject: 'Password reset token',
+      subject: 'OTP SEND',
       message,
     })
 
@@ -51,4 +52,58 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { registerUser }
+
+// @desc    Verify user email
+// @route   POST /api/v1/users/verify-email
+// @access  Public
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  const { userId, OTP } = req.body
+
+  if (!isValidObjectId(userId)) {
+    res.status(400)
+    throw new Error('Invalid user id')
+  }
+  const user = await User.findById(userId)
+  if (!user) {
+    res.status(400)
+    throw new Error('User not found')
+  }
+  if (user.isVerified) {
+    res.status(400)
+    throw new Error('User already verified')
+  }
+
+  const token = await EmailVerificationToken.findOne({ owner: userId })
+  if (!token) {
+    res.status(400)
+    throw new Error('Invalid token')
+  }
+  const isMatched = await token.compaireToken(OTP)
+  if (!isMatched) {
+    res.status(400)
+    throw new Error('Invalid OTP')
+  }
+  user.isVerified = true
+  await user.save()
+  await EmailVerificationToken.findByIdAndDelete(token._id)
+  const message = `<h1>Welcome to our app and thanks for choosing us.</h1>`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Token varify',
+      message,
+    })
+
+    res.status(200).json({
+      success: true,
+      message: `Your email is verified.`,
+    })
+  } catch (error) {
+    res.status(500)
+    throw new Error('Email could not be sent')
+  }
+})
+
+export { registerUser, verifyEmail }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { registerUser } from '../controllers/userController.js'
+import { registerUser, verifyEmail } from '../controllers/userController.js'
 import { userValidtor, validate } from '../middleware/validator.js'
 const router = express.Router()
 
 
 
 router.post('/create', userValidtor, validate, registerUser)
+router.post('/verify-email', verifyEmail)
 
 
 export default router
